c565dae1cabf38d957d791fe999f2adda2f0e07f
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2021-11-12 09:28:52 -0300

    feat: add more datasources

--- webscraping-example/webscraping_f1.py
+++ webscraping-example/webscraping_f1.py
@@ -4,14 +4,32 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 
-URL = "https://www.formula1.com/en/results.html"
+YEAR = "2021"
+
+DATASOURCES = {
+    "race": {
+        "url": f"https://www.formula1.com/en/results.html/{YEAR}/races.html",
+        "columns": ["Grand Prix", "Date", "Winner", "Car", "Laps", "Time"],
+    },
+    "drivers": {
+        "url": f"https://www.formula1.com/en/results.html/{YEAR}/drivers.html",
+        "columns": ["Pos", "Driver", "Nationality", "Car", "PTS"],
+    },
+    "team": {
+        "url": f"https://www.formula1.com/en/results.html/{YEAR}/team.html",
+        "columns": ["Pos", "Team", "PTS"],
+    },
+    "fastest_lap": {
+        "url": f"https://www.formula1.com/en/results.html/{YEAR}/fastest-laps.html",
+        "columns": ["Grand Prix", "Driver", "Car", "Time"],
+    },
+}
 
 option = Options()
 option.headless = True
 driver = webdriver.Firefox(options=option)
 
-driver.get(URL)
-driver.implicitly_wait(10)
+driver.get(DATASOURCES["fastest_lap"]["url"])
 
 element = driver.find_element(By.XPATH, "//table[@class='resultsarchive-table']")
 
@@ -22,8 +40,6 @@ driver.quit()
 soup = BeautifulSoup(html_content, "html.parser")
 table = soup.find(name="table")
 
-df_full = pd.read_html(str(table))[0].head(10)
+df_full_race = pd.read_html(str(table))[0].head(10)
 
-df = df_full[["Grand Prix", "Date", "Winner", "Car", "Laps", "Time"]]
-
-print(df)
+print(df_full_race)
