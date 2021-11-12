71e710dd1bbc338a573205bab874d1cc547cdf17
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2021-11-12 09:12:11 -0300

    feat: add webscraping for f1 site

--- webscraping-example/webscraping_f1.py
+++ webscraping-example/webscraping_f1.py
@@ -4,31 +4,26 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 
-URL = "https://stats.nba.com/players/traditional/?PerMode=Totals&Season=2019-20&SeasonType=Regular%20Season&sort=PLAYER_NAME&dir=-1"
+URL = "https://www.formula1.com/en/results.html"
 
 option = Options()
-# option.headless = True
+option.headless = True
 driver = webdriver.Firefox(options=option)
 
 driver.get(URL)
 driver.implicitly_wait(10)
 
-driver.find_element(
-    By.XPATH, "//div[@class='nba-stat-table']//table//thead//tr//th[@data-field='PTS']"
-).click()
-
-element = driver.find_element(By.XPATH, "//div[@class='nba-stat-table']//table")
+element = driver.find_element(By.XPATH, "//table[@class='resultsarchive-table']")
 
 html_content = element.get_attribute("outerHTML")
+
+driver.quit()
 
 soup = BeautifulSoup(html_content, "html.parser")
 table = soup.find(name="table")
 
 df_full = pd.read_html(str(table))[0].head(10)
 
-df = df_full[["Unnamed: 0", "PLAYER", "TEAM", "PTS"]]
-df.columns = ["pos", "player", "team", "total"]
+df = df_full[["Grand Prix", "Date", "Winner", "Car", "Laps", "Time"]]
 
 print(df)
-
-driver.quit()
